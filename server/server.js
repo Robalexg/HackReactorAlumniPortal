2c2ccd8c0c8e6db352c7df8c30bf43b630d4d724
Robalexg <alexg7070@yahoo.com>
2016-10-12 12:46:46 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var path = require("path")
+var app = express()
+
+
+
+app.get("/",function (req,res) {
+	res.sendFile(path.join(__dirname ,"../client/public/index.html"))
+})
+
+
+
+
+
+
+
+app.listen(3000)
+console.log("listening")
+
