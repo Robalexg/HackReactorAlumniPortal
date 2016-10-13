eb555cd021e4e5dd9c20b81b4c0f04698d5ef2d4
Robalexg <alexg7070@yahoo.com>
2016-10-13 11:21:43 -0500

    .gitignore works

--- server/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'facebookAuth': {
-		'clientID': '1160069764068734' 
-		'clientSecret': '448fa1dc077af73e338aa73f00ceb377',
-		 'callback': 'http:localhost:3000/auth/facebook/callback'
-	}
-}
